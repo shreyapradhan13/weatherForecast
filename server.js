1ab598266d1ab985b33e2c00007401e830e08a31
spradha1@binghamton.edu <shreyapradhan@shreyas-mbp.lan>
2020-08-01 15:56:41 -0400

    Express app setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express()
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname +'/views/index.html');
+})
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
